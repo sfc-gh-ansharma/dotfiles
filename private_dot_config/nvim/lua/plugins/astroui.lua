8afd2c1895f599b62a4b73362e889e38e9e4a625
Ankur Sharma <inbox.ankur@outlook.com>
2025-01-12 20:36:26 +0100

    Add transparency to terminal and nvim

--- private_dot_config/nvim/lua/plugins/astroui.lua
+++ private_dot_config/nvim/lua/plugins/astroui.lua
@@ -8,38 +8,34 @@ return {
   "AstroNvim/astroui",
   ---@type AstroUIOpts
   opts = {
-    -- change colorscheme
-    colorscheme = "astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = function()
         local get_hlgroup = require("astroui").get_hlgroup
         -- get highlights from highlight groups
-        local normal = get_hlgroup("Normal")
+        local normal = get_hlgroup "Normal"
         local fg, bg = normal.fg, normal.bg
         local bg_alt = get_hlgroup("Visual").bg
         local green = get_hlgroup("String").fg
         local red = get_hlgroup("Error").fg
         -- return a table of highlights for telescope based on
         -- colors gotten from highlight groups
+
         return {
-          TelescopeBorder = { fg = bg_alt, bg = bg },
-          TelescopeNormal = { bg = bg },
-          TelescopePreviewBorder = { fg = bg, bg = bg },
-          TelescopePreviewNormal = { bg = bg },
+          TelescopeBorder = { fg = bg_alt, bg = "none" },
+          TelescopeNormal = { bg = "none" },
+          TelescopePreviewBorder = { fg = bg, bg = "none" },
+          TelescopePreviewNormal = { bg = "none" },
           TelescopePreviewTitle = { fg = bg, bg = green },
           TelescopePromptBorder = { fg = bg_alt, bg = bg_alt },
           TelescopePromptNormal = { fg = fg, bg = bg_alt },
           TelescopePromptPrefix = { fg = red, bg = bg_alt },
           TelescopePromptTitle = { fg = bg, bg = red },
-          TelescopeResultsBorder = { fg = bg, bg = bg },
-          TelescopeResultsNormal = { bg = bg },
-          TelescopeResultsTitle = { fg = bg, bg = bg },
+          TelescopeResultsBorder = { fg = bg, bg = "none" },
+          TelescopeResultsNormal = { bg = "none" },
+          TelescopeResultsTitle = { fg = bg, bg = "none" },
         }
       end,
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
     },
     -- Icons can be configured throughout the interface
     icons = {
@@ -54,6 +50,11 @@ return {
       LSPLoading8 = "⠧",
       LSPLoading9 = "⠇",
       LSPLoading10 = "⠏",
+      GitAdd = "",
+    },
+    -- A table of only text "icons" used when icons are disabled
+    text_icons = {
+      GitAdd = "[+]",
     },
   },
 }
--- private_dot_config/nvim/lua/plugins/catppuccin.lua
+++ private_dot_config/nvim/lua/plugins/catppuccin.lua
@@ -6,56 +6,58 @@ return {
   },
 
   config = function()
-    require("catppuccin").setup({
-      flavour = "auto", -- latte, frappe, macchiato, mocha
+    require("catppuccin").setup {
+      flavour = "frappe", -- latte, frappe, macchiato, mocha
       background = { -- :h background
-          light = "latte",
-          dark = "frappe",
+        light = "frappe",
+        dark = "frappe",
       },
-      transparent_background = false, -- disables setting the background color.
+      transparent_background = true, -- disables setting the background color.
       show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
       term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
       dim_inactive = {
-          enabled = false, -- dims the background color of inactive window
-          shade = "dark",
-          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        enabled = true, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 2, -- percentage of the shade to apply to the inactive window
       },
       no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
+      no_bold = true, -- Force no bold
       no_underline = false, -- Force no underline
       styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-          comments = { "italic" }, -- Change the style of comments
-          conditionals = { "italic" },
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-          operators = {},
-          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
       },
       color_overrides = {},
       custom_highlights = {},
       default_integrations = true,
       integrations = {
-          cmp = true,
-          gitsigns = true,
-          nvimtree = true,
-          treesitter = true,
-          notify = false,
-          mini = {
-              enabled = true,
-              indentscope_color = "",
-          },
-          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        cmp = true,
+        fzf = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = false,
+        mini = {
+          enabled = false,
+          indentscope_color = "",
+        },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
-    })
-
-  -- setup must be called before loading
-  vim.cmd.colorscheme "catppuccin"
-  end
+    }
+    -- setup must be called before loading
+    vim.cmd.colorscheme "catppuccin"
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "LightGray", bold = false })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "LightGray", bold = false })
+  end,
 }
